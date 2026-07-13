888d1c489d1ded0ea7fecfe95d0147a5f6daa493
Satyam Pandey <84524980+SatyamPandey07@users.noreply.github.com>
2026-07-13 20:39:40 +0530

    feat: add JWT configuration settings to config.py

--- app/config.py
+++ app/config.py
@@ -2,6 +2,9 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     DATABASE_URL: str = "postgresql://postgres:postgres@localhost:5432/taskflow"
+    SECRET_KEY: str = "supersecretkey_change_me_in_production"
+    ALGORITHM: str = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
 
     model_config = SettingsConfigDict(env_file=".env")
 
