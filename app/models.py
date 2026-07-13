6ab30c7b1be021f4aca56028c9bf57949962f29b
Satyam Pandey <84524980+SatyamPandey07@users.noreply.github.com>
2026-07-13 20:29:54 +0530

    feat: configure Alembic and create initial migration schema

--- app/models.py
+++ app/models.py
@@ -6,7 +6,7 @@ from app.database import Base
 class Project(Base):
     __tablename__ = "projects"
 
-    id = Column(Integer, primary key=True, index=True)
+    id = Column(Integer, primary_key=True, index=True)
     name = Column(String, unique=True, index=True, nullable=False)
     description = Column(String, nullable=True)
     created_at = Column(DateTime(timezone=True), server_default=func.now())
@@ -17,7 +17,7 @@ class Project(Base):
 class Task(Base):
     __tablename__ = "tasks"
 
-    id = Column(Integer, primary key=True, index=True)
+    id = Column(Integer, primary_key=True, index=True)
     title = Column(String, index=True, nullable=False)
     description = Column(String, nullable=True)
     status = Column(String, default="Todo", nullable=False)
